9e628b79b47159abcb8ad2955112a1bc38385cf3
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-20 01:00:46 -0500

    Update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
+import React, { useEffect, useState } from "react";
+import Login from "./Login"
+
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me")
+      .then((r) => r.json())
+      .then((user) => setUser(user));
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
     <div className="App">
-      
+      <p>I'm in!</p>
     </div>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Header from "./Header";
+import LoginForm from "../pages/LoginForm";
+import SignUpForm from "../pages/SignUpForm";
+
+function Login({ onLogin, user, setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default Login;
