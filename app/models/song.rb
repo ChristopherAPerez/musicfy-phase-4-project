c184617ce85e326de989f7923452c0867fe2ad05
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-19 23:38:56 -0500

    Associations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ApplicationRecord
+    has_many :albums
+    has_many :users, through: :albums
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :albums
+    has_many :songs, through: :albums
+
+    validates :username, presence: true, uniqueness: true
 end
