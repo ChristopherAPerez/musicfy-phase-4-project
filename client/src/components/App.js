03a292872342218d39d107c6b021d287ac834239
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2023-01-20 01:38:47 -0500

    Update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Login from "./Login"
+import LoggedIn from "./LoggedIn"
+import LoginForm from "../pages/LoginForm"
+import SignUpForm from "../pages/SignUpForm"
 
 // import logo from './logo.svg';
 import './App.css';
@@ -7,6 +9,7 @@ import './App.css';
 function App() {
 
   const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(true);
 
   useEffect(() => {
     fetch("/me")
@@ -14,11 +17,33 @@ function App() {
       .then((user) => setUser(user));
   }, []);
 
-  if (!user) return <Login onLogin={setUser} />;
+  if (!user) return <LoggedIn />;
 
   return (
     <div className="App">
-      <p>I'm in!</p>
+
+      {showLogin ? (
+        <div>
+          <LoginForm />
+          <p>
+            Don't have an account? &nbsp;
+            <button onClick={() => setShowLogin(false)}>
+              Sign Up
+            </button>
+          </p>
+          <p>Login</p>
+        </div>
+      ) : (
+        <div>
+          <SignUpForm />
+          <p>
+            Already have an account? &nbsp;
+            <button onClick={() => setShowLogin(true)}>
+              Log In
+            </button>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ client/src/components/LoggedIn.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+// import logo from './logo.svg';
+import './App.css';
+
+function LoggedIn() {
+
+  return (
+    <div className="App">
+        I'm in!
+    </div>
+  );
+}
+
+export default LoggedIn;
